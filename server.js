7356d1c5ceba55e536d0809e29606b78578c7493
RajaM1234 <raja4507r@gmail.com>
2026-06-17 19:49:02 +0530

    Create Express server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+app.get("/users", (req, res) => {
+  res.send("Users endpoint");
+});
+app.get("/login", (req, res) => {
+  res.send("Login endpoint");
+});
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
